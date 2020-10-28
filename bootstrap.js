0dab8cd0c9e84c30a79133bf1e252e7eba951757
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-10-28 18:33:53 +0000

    Updates

--- bootstrap.js
+++ bootstrap.js
@@ -159,7 +159,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/reactor_bg.wasm":"c5fc101d8dec3a1ba9f7"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/reactor_bg.wasm":"a71923cd2c32ac091b56"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
